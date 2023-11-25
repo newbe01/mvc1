163e778e1b7eb9644aef62686ce0b5980af95907
newbe01 <pkwaa3@gmail.com>
2023-11-26 03:52:19 +0900

    model

---
+++ src/main/java/mvc1/servlet/basic/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,22 @@
+package mvc1.servlet.basic.web.frontcontroller.v4.controller;
+
+import mvc1.servlet.basic.domain.member.Member;
+import mvc1.servlet.basic.domain.member.MemberRepository;
+import mvc1.servlet.basic.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+
+        List<Member> members = memberRepository.findAll();
+
+        model.put("members", members);
+        return "members";
+    }
+}
