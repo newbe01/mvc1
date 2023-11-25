4b11a7881753ea3387e937a85efe5638731623e3
newbe01 <pkwaa3@gmail.com>
2023-11-26 07:16:30 +0900

    spring mvc

---
+++ src/main/java/mvc1/servlet/basic/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,51 @@
+package mvc1.servlet.basic.web.springmvc.v3;
+
+import mvc1.servlet.basic.domain.member.Member;
+import mvc1.servlet.basic.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/springmvc/v3/members")
+@Controller
+public class SpringMemberControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+//    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+
+//    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+                             @RequestParam("age") int age,
+                             Model model)
+    {
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+
+        return "save-result";
+    }
+
+//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.addAttribute("members", members);
+
+        return "members";
+    }
+
+
+
+}
