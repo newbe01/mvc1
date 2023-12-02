b609be163cba21efd4cb2cbe7885f4b7763a3f99
newbe01 <pkwaa3@gmail.com>
2023-12-02 22:52:25 +0900

    addItem

--- item-service/src/main/java/tmp/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/tmp/itemservice/web/basic/BasicItemController.java
@@ -4,9 +4,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import tmp.itemservice.domain.item.Item;
 import tmp.itemservice.domain.item.ItemRepository;
 
@@ -39,6 +37,36 @@ public class BasicItemController {
     @GetMapping("/add")
     public String addForm() {
         return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(
+            @RequestParam("itemName") String itemName,
+            @RequestParam("price") int price,
+            @RequestParam("quantity") Integer quantity,
+            Model model)
+    {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+//    public String addItemV2(@ModelAttribute("item") Item item, Model model) { > 아래로 변경가능
+//    public String addItemV2(@ModelAttribute Item item) {
+    public String addItemV2(Item item) {    // modelAttribute 생략 가능
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); modelAttribute 에서 처리해줌
+
+        return "basic/item";
     }
 
 
