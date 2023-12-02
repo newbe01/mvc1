dd7b2ac56b62470c2f2489f73c6fefca273a835d
newbe01 <pkwaa3@gmail.com>
2023-12-02 22:29:24 +0900

    item detail

--- item-service/src/main/java/tmp/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/tmp/itemservice/web/basic/BasicItemController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tmp.itemservice.domain.item.Item;
 import tmp.itemservice.domain.item.ItemRepository;
@@ -25,6 +26,16 @@ public class BasicItemController {
         model.addAttribute("items", items);
         return "basic/items";
     }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") long itemId, Model model) {
+
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
 
     @PostConstruct
     public void init() {
